540e9bdcf44d4757407dd397f4648301eda3bcdc
Egor <Egx777@yandex.ru>
2022-10-29 10:58:13 +0300

    //goooo

--- wiki.py
+++ wiki.py
@@ -1,18 +1,19 @@
 import wikipedia
 
-wikipedia.set_lang("en")
+wikipedia.set_lang("ru")
 
 
 def search_wiki(word):
+
     try:
         w = wikipedia.search(word)
         if w:
             w1 = wikipedia.summary(word)
-            w2 = wikipedia.page((word).url)
+            w2 = wikipedia.page(word).url
             return w1, "\nLink: " + w2
-        return "Request its not find", ""
+        return "Request its not found", ""
     except:
-        return "None"
+        return "None", ""
 
 
 
